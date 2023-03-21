b5ff1d30f38792849d485e36665096d5e69ba9d1
Frdyrkuu <socorinzyk@gmail.com>
2023-03-21 20:32:13 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "*.html"],
+  theme: {
+    extend: {
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        },
+        slowspin: {
+          '0%': {
+            transform: 'rotate(0deg)'
+          },
+          '100': {
+            transform: 'rotate(360deg)'
+          },
+        }
+      },
+
+      animation: {
+        'waving-hand': 'wave 2s linear infinite',
+        'slowspin': 'slowspin 5s linear infinite',
+      },
+
+      
+
+    },
+  },
+  plugins: [],
+}
