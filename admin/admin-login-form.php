393437b95f71242a00381c3a4f3a61d068960c72
Frdyrkuu <socorinzyk@gmail.com>
2023-03-24 17:34:24 +0800

    loginform

--- admin/admin-login-form.php
+++ admin/admin-login-form.php
@@ -59,8 +59,37 @@
 
     <main class="max-w-7xl mx-auto">
         <!-- START OF LANDING PAGE -->
-        <section class="font-['Poppins'] flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-4 mb-12f">
-          
+        <section class="font-['Poppins'] p-6 ">
+            <div class="">
+                <div class="max-w-lg rounded-2xl overflow-hidden shadow-xl m-auto">
+                    <img class="w-60 m-auto" src="../public/images/logo-icon.png" alt="Sunset in the mountains">
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-5 text-center text-green-900">Administrive Login Form</div>
+
+                        <div class="mb-4">
+                            <label class="block text-gray-700 font-bold mb-2" for="username">
+                                Username
+                            </label>
+                            <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter your username" required>
+                        </div>
+                        <div class="mb-6">
+                            <label class="block text-gray-700 font-bold mb-2" for="password">
+                                Password
+                            </label>
+                            <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter your password" required>
+
+                            
+                        </div>
+
+                    </div>
+                    <div class="px-6 py-4">
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag3</span>
+                    </div>
+                </div>
+
+            </div>
         </section>
         <!-- END LANDING PAGE -->
     </main>
