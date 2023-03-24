47a3400ffbf8d7c26370e281d8742c39546af4cb
Frdyrkuu <socorinzyk@gmail.com>
2023-03-24 17:53:01 +0800

    admin-updates

--- admin/admin-login-form.php
+++ admin/admin-login-form.php
@@ -8,7 +8,7 @@
     <title>Eco-Cyle</title>
 </head>
 
-<body class="body min-h-screen">
+<body class="body min-h-screen font-['Poppins']">
 
     <!-- HEADER -->
     <header class="bg-white text-green-900 sticky top-0 z-50 shadow-md rounded-2xl">
@@ -16,7 +16,7 @@
             <h1 class="text-4xl font-extrabold font-['Poppins']">
                 <div class="flex items-center hover:opacity-70">
                     <img src="../public/images/logo-icon.png" alt="" class="w-24">
-                    <a href="" class=""><span class="text-green-500">Ec</span>o-Cyle</a>
+                    <a href="../index.html" class=""><span class="text-green-500">Ec</span>o-Cyle</a>
                 </div>
             </h1>
             <div>
@@ -35,7 +35,7 @@
             </div>
         </section>
 
-        <section id="hamburger" class="bg-white w-full rounded-2xl shadow-md shadow-bottom-2xl -mt-5 absolute hidden text-center animate-reveal">
+        <section id="hamburger" class="bg-white w-full rounded-2xl shadow-md shadow-bottom-2xl -mt-5 absolute hidden text-center">
             <nav>
                 <div class="">
                     <div class="text-green-900 px-2 pt-2 pb-3 space-y-1 sm:px-3">
@@ -44,10 +44,6 @@
                             Goal</a>
                         <a href="#" class="block px-3 py-2 rounded-md text-base font-medium hover:bg-green-200">About
                             Us</a>
-                        <a href="admin/admin-login-form.php">
-                            <button class="p-2 pl-5 pr-5 border-2 bg-green-500 text-white font-bold rounded-xl transition-colors duration-700 transform hover:bg-green-700 hover:text-gray-100 focus:border-4 focus:border-green-300 border-b-8 border-green-900 hover:scale-105">System
-                                Administrator</button>
-                        </a>
                     </div>
                 </div>
             </nav>
@@ -66,27 +62,29 @@
                     <div class="px-6 py-4">
                         <div class="font-bold text-xl mb-5 text-center text-green-900">Administrive Login Form</div>
 
-                        <div class="mb-4">
-                            <label class="block text-gray-700 font-bold mb-2" for="username">
-                                Username
-                            </label>
-                            <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter your username" required>
+                        <div class="flex flex-col space-y-4 font-semibold">
+                            <div>
+                                <label for="username" class="text-gray-700 font-bold">
+                                    Username
+                                </label>
+                                <input type="text" id="username" name="username" class="w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:shadow-outline">
+                            </div>
+                            <div>
+                                <label for="password" class="text-gray-700 font-bold">
+                                    Password
+                                </label>
+                                <input type="password" id="password" name="password" class="w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:shadow-outline">
+                            </div>
+                            <div class="m-auto">
+                                <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-20 rounded-2xl focus:outline-none hover:scale-105 focus:shadow-outline">
+                                    Sign In
+                                </button>
+                            </div>
                         </div>
-                        <div class="mb-6">
-                            <label class="block text-gray-700 font-bold mb-2" for="password">
-                                Password
-                            </label>
-                            <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter your password" required>
 
-                            
-                        </div>
 
                     </div>
-                    <div class="px-6 py-4">
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag3</span>
-                    </div>
+
                 </div>
 
             </div>
@@ -94,12 +92,11 @@
         <!-- END LANDING PAGE -->
     </main>
 
-<!-- 
     <footer class="bg-gray-100 py-4 bottom-0 w-full rounded-2xl shadow-xl">
         <div class="container mx-auto text-center">
             <p class="text-gray-900 text-sm">© 2023 EcoCycle. All rights reserved.</p>
         </div>
-    </footer> -->
+    </footer>
     <script src="../src/hamburger.js"></script>
 </body>
 
